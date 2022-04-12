a50fa99f38376dbb699697871de3ae08d8f0e34a
taoufik ahjam <taoufikahjam@gmail.com>
2022-04-12 13:47:55 +0200

    first commit example1 : Counter

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Counter from './features/counter/Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <Counter />
+   
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/app/store.js
@@ -0,0 +1,12 @@
+import {configureStore} from '@reduxjs/toolkit';
+import  counterSlice  from './../features/counter/counterSlice';
+
+
+export const store=configureStore({
+    reducer:{
+        counter:counterSlice,
+     
+
+ 
+    }
+});
